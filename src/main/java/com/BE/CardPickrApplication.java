dab23a7f13b408ef15bd996cec5979edd143f0d2
EUNSEMIN <dmstpals104@ajou.ac.kr>
2025-01-19 23:08:31 +0900

    feat : add opencsv and apply card data parsing service

--- src/main/java/com/BE/CardPickrApplication.java
+++ src/main/java/com/BE/CardPickrApplication.java
@@ -1,10 +1,11 @@
 package com.BE;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class CardPickrApplication {
 
 	public static void main(String[] args) {
